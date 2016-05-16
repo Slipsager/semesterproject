9065f2b18d2e17557fb1c9c3177a33b446958976
Vigan Shemsiu <vigan@shemsiu.dk>
2016-05-16 20:10:56 +0200

    Testimonials styled

--- modules/field/theme/field.tpl.php
+++ modules/field/theme/field.tpl.php
@@ -59,4 +59,4 @@ HTML comment.
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
     <?php endforeach; ?>
   </div>
-</div>
+</div> - vigan
